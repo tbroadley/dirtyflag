0afed4a18bf88432adddf23a16be9ec1642321ed
Thomas Broadley <buriedunderbooks@hotmail.com>
2023-11-19 00:45:15 -0800

    Add simple login page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
+from flask import session
 from flask import url_for
 
 from dotenv import load_dotenv
@@ -29,7 +30,27 @@ oauth.register("lichess", client_kwargs={"code_challenge_method": "S256"})
 stockfish = Stockfish("./stockfish/src/stockfish")
 
 
+def get_berserk_client(access_token: str) -> berserk.Client:
+    berserk_session = berserk.TokenSession(access_token)
+    return berserk.Client(session=berserk_session)
+
+
+def get_username(berserk_client: berserk.Client) -> str:
+    return berserk_client.account.get()["username"]
+
+
 @app.route("/")
+def index():
+    if "token" not in session:
+        return render_template("index.html", username=None)
+
+    berserk_client = get_berserk_client(session["token"]["access_token"])
+    username = get_username(berserk_client)
+
+    return render_template("index.html", username=username)
+
+
+@app.route("/login")
 def login():
     redirect_uri = url_for("authorize", _external=True)
     """
@@ -41,14 +62,18 @@ def login():
     return oauth.lichess.authorize_redirect(redirect_uri)
 
 
-@app.route("/authorize")
+@app.route("/api/authorize")
 def authorize():
     token = oauth.lichess.authorize_access_token()
-    bearer = token["access_token"]
+    session["token"] = token
 
-    berserk_session = berserk.TokenSession(bearer)
-    berserk_client = berserk.Client(session=berserk_session)
 
+@app.route("/api/dirty-flags")
+def get_dirty_flags():
+    if "token" not in session:
+        return jsonify({}), 401
+
+    berserk_client = get_berserk_client(session["token"]["access_token"])
     username = berserk_client.account.get()["username"]
 
     games = get_games(berserk_client, username)
